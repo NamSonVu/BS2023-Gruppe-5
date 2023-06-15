8d21673c51abb8adc09b12e1fe49f439fdeb3b7a
Nam Son Vu <nam_son.vu@smail.th-koeln.de>
2023-06-15 12:56:54 +0200

    newest1

---
+++ cmake-build-debug-dockervm-ubuntu-1804/CMakeFiles/TestRM.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/TestRM.dir/main.c.o"
+  "CMakeFiles/TestRM.dir/keyValStore.c.o"
+  "CMakeFiles/TestRM.dir/sub.c.o"
+  "TestRM.pdb"
+  "TestRM"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/TestRM.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
